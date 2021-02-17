46150d12a9bb6bd16f60c4a705fe309df37ffb8e
Àlex Cervilla <cervilla.alex@gmail.com>
2021-02-17 23:46:34 +0100

    PuzzleGeneralEditor, PuzzleGdsParsers and some reformatting

--- GUI/generated/PuzzleGeneralEditor.py
+++ GUI/generated/PuzzleGeneralEditor.py
@@ -11,17 +11,17 @@ import wx
 import wx.xrc
 
 ###########################################################################
-## Class PuzzleInputEditor
+## Class PuzzleGeneralEditor
 ###########################################################################
 
-class PuzzleInputEditor ( wx.Frame ):
+class PuzzleGeneralEditor ( wx.Frame ):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 793,467 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Puzzle GDS Editor", pos = wx.DefaultPosition, size = wx.Size( 1026,583 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
-		bSizer71 = wx.BoxSizer( wx.HORIZONTAL )
+		bSizer70 = wx.BoxSizer( wx.HORIZONTAL )
 
 		self.m_panel28 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
 		self.m_panel28.SetForegroundColour( wx.Colour( 0, 0, 0 ) )
@@ -73,64 +73,68 @@ class PuzzleInputEditor ( wx.Frame ):
 		self.m_panel28.SetSizer( bSizer54 )
 		self.m_panel28.Layout()
 		bSizer54.Fit( self.m_panel28 )
-		bSizer71.Add( self.m_panel28, 1, wx.EXPAND |wx.ALL, 5 )
+		bSizer70.Add( self.m_panel28, 3, wx.EXPAND |wx.ALL, 5 )
 
 		self.m_panel36 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
 		self.m_panel36.SetForegroundColour( wx.Colour( 0, 0, 0 ) )
 		self.m_panel36.SetBackgroundColour( wx.Colour( 224, 224, 224 ) )
 
-		bSizer80 = wx.BoxSizer( wx.VERTICAL )
+		bSizer77 = wx.BoxSizer( wx.VERTICAL )
 
-		self.input_bg_preview = wx.StaticBitmap( self.m_panel36, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.Size( 256,256 ), 0 )
-		bSizer80.Add( self.input_bg_preview, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
+		bSizer78 = wx.BoxSizer( wx.HORIZONTAL )
 
-		bSizer81 = wx.BoxSizer( wx.HORIZONTAL )
+		self.commands_lbl = wx.StaticText( self.m_panel36, wx.ID_ANY, u"Commands", wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_CENTER_HORIZONTAL )
+		self.commands_lbl.Wrap( -1 )
 
-		self.input_bg_lbl = wx.StaticText( self.m_panel36, wx.ID_ANY, u"Input BG: ", wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_RIGHT )
-		self.input_bg_lbl.Wrap( -1 )
+		bSizer78.Add( self.commands_lbl, 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
 
-		bSizer81.Add( self.input_bg_lbl, 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+		self.button_cmd_new = wx.Button( self.m_panel36, wx.ID_ANY, u"New", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer78.Add( self.button_cmd_new, 1, wx.ALL, 5 )
 
-		self.input_bg_inp = wx.TextCtrl( self.m_panel36, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer81.Add( self.input_bg_inp, 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+		self.button_cmd_delete = wx.Button( self.m_panel36, wx.ID_ANY, u"Delete", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer78.Add( self.button_cmd_delete, 1, wx.ALL, 5 )
 
+		self.button_cmd_up = wx.Button( self.m_panel36, wx.ID_ANY, u"Move up", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer78.Add( self.button_cmd_up, 1, wx.ALL, 5 )
 
-		bSizer80.Add( bSizer81, 0, wx.ALIGN_CENTER, 5 )
-
-		bSizer82 = wx.BoxSizer( wx.HORIZONTAL )
-
-		self.type_of_input_lbl = wx.StaticText( self.m_panel36, wx.ID_ANY, u"Type of input: ", wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_RIGHT )
-		self.type_of_input_lbl.Wrap( -1 )
-
-		bSizer82.Add( self.type_of_input_lbl, 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
-
-		self.type_of_input_inp = wx.TextCtrl( self.m_panel36, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer82.Add( self.type_of_input_inp, 1, wx.ALL, 5 )
+		self.button_cmd_down = wx.Button( self.m_panel36, wx.ID_ANY, u"Move down", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer78.Add( self.button_cmd_down, 1, wx.ALL, 5 )
 
 
-		bSizer80.Add( bSizer82, 0, wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		bSizer77.Add( bSizer78, 0, wx.EXPAND, 5 )
 
-		bSizer83 = wx.BoxSizer( wx.HORIZONTAL )
+		self.command_list = wx.ListCtrl( self.m_panel36, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LC_LIST )
+		bSizer77.Add( self.command_list, 1, wx.ALL|wx.EXPAND, 5 )
 
-		self.answer_lbl = wx.StaticText( self.m_panel36, wx.ID_ANY, u"Answer: ", wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_RIGHT )
-		self.answer_lbl.Wrap( -1 )
+		parameter_sizer = wx.BoxSizer( wx.VERTICAL )
 
-		bSizer83.Add( self.answer_lbl, 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+		self.parameter_lbl = wx.StaticText( self.m_panel36, wx.ID_ANY, u"Parameters", wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_CENTER_HORIZONTAL )
+		self.parameter_lbl.Wrap( -1 )
 
-		self.answer_inp = wx.TextCtrl( self.m_panel36, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer83.Add( self.answer_inp, 1, wx.ALL, 5 )
+		parameter_sizer.Add( self.parameter_lbl, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
+
+		bSizer771 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.param_num_inp = wx.TextCtrl( self.m_panel36, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer771.Add( self.param_num_inp, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
+
+		self.button_set_param_num = wx.Button( self.m_panel36, wx.ID_ANY, u"Set number of parameters", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer771.Add( self.button_set_param_num, 1, wx.ALL, 5 )
 
 
-		bSizer80.Add( bSizer83, 0, wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		parameter_sizer.Add( bSizer771, 0, wx.EXPAND, 5 )
 
 
-		self.m_panel36.SetSizer( bSizer80 )
+		bSizer77.Add( parameter_sizer, 1, wx.EXPAND, 5 )
+
+
+		self.m_panel36.SetSizer( bSizer77 )
 		self.m_panel36.Layout()
-		bSizer80.Fit( self.m_panel36 )
-		bSizer71.Add( self.m_panel36, 1, wx.EXPAND |wx.ALL, 5 )
+		bSizer77.Fit( self.m_panel36 )
+		bSizer70.Add( self.m_panel36, 4, wx.EXPAND |wx.ALL, 5 )
 
 
-		self.SetSizer( bSizer71 )
+		self.SetSizer( bSizer70 )
 		self.Layout()
 
 		self.Centre( wx.BOTH )
@@ -139,6 +143,12 @@ class PuzzleInputEditor ( wx.Frame ):
 		self.gds_load_button.Bind( wx.EVT_BUTTON, self.OnButtonGDSLoad )
 		self.gds_save_button.Bind( wx.EVT_BUTTON, self.OnButtonGDSSave )
 		self.m_button43.Bind( wx.EVT_BUTTON, self.OnButtonUpdatePuzzlePreview )
+		self.button_cmd_new.Bind( wx.EVT_BUTTON, self.OnCmdNew )
+		self.button_cmd_delete.Bind( wx.EVT_BUTTON, self.OnCmdDel )
+		self.button_cmd_up.Bind( wx.EVT_BUTTON, self.OnCmdUp )
+		self.button_cmd_down.Bind( wx.EVT_BUTTON, self.OnCmdDown )
+		self.command_list.Bind( wx.EVT_LIST_ITEM_SELECTED, self.OnCommandListSelected )
+		self.button_set_param_num.Bind( wx.EVT_BUTTON, self.OnParamSetNum )
 
 	def __del__( self ):
 		pass
@@ -152,6 +162,24 @@ class PuzzleInputEditor ( wx.Frame ):
 		event.Skip()
 
 	def OnButtonUpdatePuzzlePreview( self, event ):
+		event.Skip()
+
+	def OnCmdNew( self, event ):
+		event.Skip()
+
+	def OnCmdDel( self, event ):
+		event.Skip()
+
+	def OnCmdUp( self, event ):
+		event.Skip()
+
+	def OnCmdDown( self, event ):
+		event.Skip()
+
+	def OnCommandListSelected( self, event ):
+		event.Skip()
+
+	def OnParamSetNum( self, event ):
 		event.Skip()
 
 
