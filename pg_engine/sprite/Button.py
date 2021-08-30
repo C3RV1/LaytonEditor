564836a777ae90295db94be379612b036191b273
Àlex Cervilla <cervilla.alex@gmail.com>
2021-08-31 00:19:53 +0200

    Button remove debug and fixes

--- pg_engine/sprite/Button.py
+++ pg_engine/sprite/Button.py
@@ -1,10 +1,14 @@
 from .Sprite import Sprite
 from ..Input import Input
 from ..Camera import Camera
+import pygame as pg
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .SpriteLoader import SpriteLoader
 
 
 class Button(Sprite):
-    def __init__(self, *args, pressed_tag=None, not_pressed_tag=None, pressed_counter=0.2, **kwargs):
+    def __init__(self, *args, pressed_tag=None, not_pressed_tag=None, pressed_counter=0.1, **kwargs):
         super(Button, self).__init__(*args, **kwargs)
         self._pressed_tag = pressed_tag
         self._not_pressed_tag = not_pressed_tag
@@ -14,6 +18,10 @@ class Button(Sprite):
         self.input_manager = Input()
         if self._not_pressed_tag:
             self.set_tag(self._not_pressed_tag)
+
+    def load_sprite(self, loader: 'SpriteLoader', surface: pg.Surface, frame_info, tag_info, vars_=None):
+        super(Button, self).load_sprite(loader, surface, frame_info, tag_info, vars_=vars_)
+        self.set_tag(self._not_pressed_tag)
 
     def pressed(self, cam: Camera, dt: float):
         if not self._pressed:
@@ -25,16 +33,33 @@ class Button(Sprite):
                         self.set_tag(self._pressed_tag)
                     self._current_pressed_counter = self.pressed_counter
                     return False
-            if self._not_pressed_tag:
-                self.set_tag(self._not_pressed_tag)
         else:
             if self._current_pressed_counter is None:
                 self._pressed = False
                 return True
             if self._current_pressed_counter <= 0:
                 self.set_tag(self._not_pressed_tag)
-                print("OFF")
                 self._current_pressed_counter = None
                 return False
             self._current_pressed_counter -= dt
         return False
+
+    @property
+    def not_pressed_tag(self):
+        return self._not_pressed_tag
+
+    @not_pressed_tag.setter
+    def not_pressed_tag(self, v: str):
+        self._not_pressed_tag = v
+        if not self._pressed:
+            self.set_tag(v)
+
+    @property
+    def pressed_tag(self):
+        return self._pressed_tag
+
+    @pressed_tag.setter
+    def pressed_tag(self, v: str):
+        self._pressed_tag = v
+        if self._pressed:
+            self.set_tag(v)
