c4d9fb5fa224aaac7feec05f40c106e15444bb76
Àlex Cervilla <cervilla.alex@gmail.com>
2022-12-12 10:58:01 +0100

    BackgroundWidgetUI

---
+++ gui/ui/BackgroundWidget.py
@@ -0,0 +1,13 @@
+from PySide6 import QtCore, QtWidgets, QtGui
+
+
+class BackgroundWidgetUI(QtWidgets.QWidget):
+    def __init__(self, *args, **kwargs):
+        super(BackgroundWidgetUI, self).__init__(*args, **kwargs)
+
+        self.v_layout = QtWidgets.QVBoxLayout()
+
+        self.image_preview = QtWidgets.QLabel()
+        self.v_layout.addWidget(self.image_preview)
+
+        self.setLayout(self.v_layout)
