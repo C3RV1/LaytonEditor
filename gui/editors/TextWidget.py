492149db14982b2d8f4d688cc1ac924a7c259871
Àlex Cervilla <cervilla.alex@gmail.com>
2023-02-11 11:06:06 +0100

    text shift-jis saving

--- gui/editors/TextWidget.py
+++ gui/editors/TextWidget.py
@@ -19,14 +19,18 @@ class TextEditor(TextWidgetUI):
         data = f.read()
         try:
             self.text_editor.setPlainText(data.decode("cp1252"))
-            self.save_btn.setEnabled(True)
+            self.encoding_combobox.setCurrentIndex(0)
         except UnicodeDecodeError:
             self.text_editor.setPlainText(data.decode("shift-jis"))
-            self.save_btn.setEnabled(False)
+            self.encoding_combobox.setCurrentIndex(1)
         finally:
             f.close()
 
     def save_btn_click(self):
-        f = self.text_asset.rom.open(self.text_asset.path, "w")
-        f.write(self.text_editor.toPlainText())
+        f = self.text_asset.rom.open(self.text_asset.path, "wb")
+        text = self.text_editor.toPlainText()
+        if self.encoding_combobox.currentIndex() == 0:
+            f.write(text.encode("cp1252"))
+        elif self.encoding_combobox.currentIndex() == 1:
+            f.write(text.encode("shift-jis"))
         f.close()
--- gui/ui/TextWidget.py
+++ gui/ui/TextWidget.py
@@ -9,11 +9,23 @@ class TextWidgetUI(QtWidgets.QWidget):
 
         self.text_editor = QtWidgets.QPlainTextEdit(self)
 
+        self.end_h_layout = QtWidgets.QHBoxLayout()
+
         self.save_btn = QtWidgets.QPushButton("Save")
         self.save_btn.clicked.connect(self.save_btn_click)
+        self.end_h_layout.addWidget(self.save_btn, 2)
+
+        self.encoding_label = QtWidgets.QLabel("Encoding: ")
+        self.encoding_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
+        self.end_h_layout.addWidget(self.encoding_label, 1)
+
+        self.encoding_combobox = QtWidgets.QComboBox()
+        self.encoding_combobox.addItem("CP1252 (EU)")
+        self.encoding_combobox.addItem("Shift-jis (JP)")
+        self.end_h_layout.addWidget(self.encoding_combobox, 1)
 
         self.v_layout.addWidget(self.text_editor, 4)
-        self.v_layout.addWidget(self.save_btn, 1)
+        self.v_layout.addLayout(self.end_h_layout, 1)
 
         self.setLayout(self.v_layout)
 
