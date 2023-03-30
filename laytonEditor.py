b19203b379e7643fc980bf9f2b08a71f9c3521ae
Àlex Cervilla <cervilla.alex@gmail.com>
2023-03-30 19:03:14 +0200

    fix bundling part 2

--- laytonEditor.py
+++ laytonEditor.py
@@ -2,11 +2,13 @@ import logging
 from utility.logger import set_up_logger
 import sys
 import os
+import multiprocessing
 # import faulthandler
 
 VERSION = "v0.5-pre1"
 
 if __name__ == '__main__':
+    multiprocessing.freeze_support()
     os.chdir(os.path.dirname(__file__))  # Ensure that the cwd is set correctly
     set_up_logger()  # Set logger up before importing to log import errors
     logging.info(f"\n\nLayton Editor {VERSION} running in python version {sys.version}")
